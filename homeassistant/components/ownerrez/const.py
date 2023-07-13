a4707a65eaf5119e699584ef2ff38c8576a7bb61
blueflame66 <glenn.moore@gmail.com>
2023-07-13 14:12:16 -0400

    Add in API Work
    Add in API work

--- homeassistant/components/ownerrez/const.py
+++ homeassistant/components/ownerrez/const.py
@@ -1,3 +1,12 @@
-"""Constants for the Owner Reservations integration."""
+"""Constants for the OwnerRez integration."""
 
 DOMAIN = "ownerrez"
+
+CONF_DAYS = "days"
+
+DEFAULT_URL = "https://secure.ownerreservations.com/api"
+DEFAULT_PT = ""
+DEFAULT_UN = ""
+
+OR_URL1 = "https://secure.ownerreservations.com/api"
+OR_URL2 = "https://api.ownerreservations.com/v2"
